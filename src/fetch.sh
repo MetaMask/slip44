a00c75377f9ac1962fd7a52c91168750392a20de
Erik Marks <rekmarks@protonmail.com>
2022-05-04 22:07:30 -0700

    fixup! Update README

--- src/fetch.sh
+++ src/fetch.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-curl https://raw.githubusercontent.com/satoshilabs/slips/master/slip-0044.md >> src/slip44.md
+curl https://raw.githubusercontent.com/satoshilabs/slips/master/slip-0044.md > src/slip44.md
 
