5de55f59a88227d876ea28c3d1ad154f4adef449
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2022-05-07 23:14:32 -0700

    Fix build scripts (#5)
    The `prepublishOnly` script was broken due to poorly composed build scripts. In particular, `build:clean` tried to remove the same file twice, causing an error. While fixing that issue, I decided to refactor the build scripts.
    
    This PR refactors the build scripts to only minimally use Bash, and to not break during releases. In addition, the raw SLIP-44 data is now committed to this repository, both for clerical purposes and in order to stop fetching it in CI. Finally, a clean working directory check is added to CI after building from the committed markdown.

---
+++ src/fetch.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+curl https://raw.githubusercontent.com/satoshilabs/slips/master/slip-0044.md > src/slip44.md
+
--- src/generate.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-rm slip44.json
-curl https://raw.githubusercontent.com/satoshilabs/slips/master/slip-0044.md >> src/slip44.md
-node src/parse.js >> slip44.json
