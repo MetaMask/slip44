c897cad4a6e239c7203c06c4a2dd26bc26bd5433
Erik Marks <rekmarks@protonmail.com>
2022-08-07 16:38:41 -0700

    Fix PR creation

--- scripts/fetch-rebuild-push.sh
+++ scripts/fetch-rebuild-push.sh
@@ -23,16 +23,20 @@ yarn build:clean
 yarn lint:fix
 yarn test
 
+BRANCH_NAME="update-slip44-${CURRENT_DATE}"
 PR_TITLE="Update with latest data as of ${CURRENT_DATE}"
 
 git config user.name "github-actions[bot]"
 git config user.email "github-actions[bot]@users.noreply.github.com"
 
-git checkout -b "update-slip44-${CURRENT_DATE}"
+git checkout -b "${BRANCH_NAME}"
 
 git add .
 git commit -m "${PR_TITLE}"
 
+git push --set-upstream origin "${BRANCH_NAME}"
+
 gh pr create \
   --title "${PR_TITLE}" \
+  --head "${BRANCH_NAME}" \
   --body "Updates the package with the latest SLIP-44 data as of ${CURRENT_DATE}." 
