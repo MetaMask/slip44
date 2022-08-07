bf5f5ba9d49258c6e3a9b9fae69a788b76d746a3
Erik Marks <rekmarks@protonmail.com>
2022-08-07 16:17:44 -0700

    Create pull request instead of pushing to main

--- scripts/fetch-rebuild-push.sh
+++ scripts/fetch-rebuild-push.sh
@@ -23,9 +23,16 @@ yarn build:clean
 yarn lint:fix
 yarn test
 
+PR_TITLE="Update with latest data as of ${CURRENT_DATE}"
+
 git config user.name "github-actions[bot]"
 git config user.email "github-actions[bot]@users.noreply.github.com"
 
+git checkout -b "update-slip44-${CURRENT_DATE}"
+
 git add .
-git commit -m "Update with latest data as of ${CURRENT_DATE}"
-git push -u origin main
+git commit -m "${PR_TITLE}"
+
+gh pr create \
+  --title "${PR_TITLE}" \
+  --body "Updates the package with the latest SLIP-44 data as of ${CURRENT_DATE}." 
