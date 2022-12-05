0312bc4cb1d1024bb25f3082ca3a588acf081b21
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-12-05 11:38:28 +0100

    Bump ESLint configs and related dependencies

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,6 +5,7 @@ describe('Generated SLIP-44 JSON file', () => {
     expect(Object.keys(slip44Json).length).toBeGreaterThan(1);
     expect(slip44Json[60]).toStrictEqual({
       index: '60',
+      // eslint-disable-next-line id-denylist
       hex: '0x8000003c',
       symbol: 'ETH',
       name: 'Ethereum',
